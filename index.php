09fc886f36888669aa687e331af28f88c114c6d7
Pedro Ryago <pedrorbn121@gmail.com>
2023-01-12 15:54:19 -0300

    feat: nodePort service created

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>MyApp PHP 1.0</title>
+ </head>
+ <body>
+    <h1>MyApp PHP 1.0</h1>
+ <?php 
+
+ echo gethostname();
+ echo "<br>";
+ echo date('Y-m-d H:i:s');
+ 
+ ?>
+ </body>
+</html>
